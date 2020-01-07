2e716353f56d869475bbd69b30233d1ab204290c
aether-fiducia <aethersaunders@gmail.com>
2020-01-07 14:14:20 -0400

    Manage use and method completion

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ extern crate rand;
 
 use rand::Rng;
 
+use std::convert::TryFrom;
+
+use std::io::{self, stdout, Read, Write};
+
+use termion::{clear, cursor, raw};
+
+use std::thread;
+
+use std::process;
+
+use std::time::Duration;
+
 // A structure to hold the character buffers and what parts are visable
 struct CharStream {
     pub sequence: String,
@@ -11,26 +23,43 @@ struct CharStream {
 
 impl CharStream {
 
+    // Gen a new full CharStream given a length
     fn new(length: u16) -> Self {
         let mut result = Self {sequence: String::new(), visable: (0,0)};
-        // The rng thread needs to only print valid characters!
-        // Make it happen baby
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::thread_rng(); // Not secure but fast rng 
         for _k in 0..length {
-            result.sequence.push(rng.gen());
+            result.sequence.push( // 4. Add random char to the sequence
+                char::try_from( // 3. Use u32 as standard latin character
+                    rng.gen_range(0x0020, 0x007F) as u32) // 1. Gen random <u32,error>
+                .unwrap()); // 2. Discard error
         }
         return result;
+    }
+
+    // Increase max tail visability
+    fn grow(&mut self) {
+        self.visable.1 += 1;
+    }
+
+    //Decrease head visability
+    fn shrink(&mut self) {
+        self.visable.0 += 1;
     }
 
 }
 
 fn main() {
-    println!("Well shit it works");
+    let (x_tty_size, y_tty_size) = termion::terminal_size().unwrap();
 }
 
-#[test]
-fn test() {
-    let test_bio = CharStream::new(2);
-    println!("{}", test_bio.sequence);
-    println!("{}", test_bio.visable.1);
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut test_bio = CharStream::new(2);
+        test_bio.visable.1 = 18;
+    }
 }
