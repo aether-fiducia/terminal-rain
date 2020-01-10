ae7b2405775918e72f595a95220aa1bedff19a80
aether-fiducia <aethersaunders@gmail.com>
2020-01-10 11:55:50 -0400

    Working events

--- src/event.rs
+++ src/event.rs
@@ -12,54 +12,55 @@ pub enum Event {
     Exit,
 }
 
-/* Make events struct with a Receiver for Event(s) an input handler and tick handler.
- * The receiver wiill moniter a mpsc channel with thhe sender being used by the handler threads.
+/* Make events struct with a Receiver for Event(s) an input handle and tick handle.
+ * The receiver wiill moniter a mpsc channel with thhe sender being used by the handle threads.
  * The input thread waits for a Ctrl+C from the user.
  * The tick thread moniters the tick rate.
  */
 pub struct Events {
     rx: mpsc::Receiver<Event>,
-    input_handle: thread::JoinHandler<()>,
-    tick_handler: thread::JoinHandler<()>,
+    input_handle: thread::JoinHandle<()>,
+    tick_handle: thread::JoinHandle<()>,
 }
 
 impl Events {
     pub fn new(tick_rate: u64) -> Self {
         // Open a sender and receiver for the Events receiver and two threads
         let (tx, rx) = mpsc::channel();
-        /* Make the input handler by:
+        /* Make the input handle by:
          * One-> Cloning the sending channel
          * Two-> Spawning a new thread
          * Three-> Seeing if Crtl+C was pressed by the user
          * (Four)-> Sending the Exit Event should that occur
          */
-        let input_handler = {
+        let input_handle = {
             let tx = tx.clone();
             thread::spawn (move || {
                 let stdin = io::stdin();
-                if stdin.keys().any(|x| x == Ok(Key::Crtl('c'))) {
+                if stdin.keys().any(|x| if let Ok(Key::Ctrl('c')) = x {true}
+                                    else {false}) {
                     tx.send(Event::Exit).unwrap();
                 }
             })
         };
-        /* Make the tick handler by:
+        /* Make the tick handle by:
          * One-> Cloning the sending channel
          * Two-> Spawning a new looping thread (loops for duration of main thread)
          * Three-> Sending a Tick Event to the Receiver
          * Four-> Pausing for the supplied tick rate amount of time
          */
-        let tick_handler = {
+        let tick_handle = {
             let tx = tx.clone();
             thread::spawn(move || loop {
-                tx.send(Event.Tick).unwrap();
+                tx.send(Event::Tick).unwrap();
                 thread::sleep(Duration::from_millis(tick_rate));
             })
         };
         // Return Events
         Events {
             rx,
-            input_handler: input_handler,
-            tick_handler: tick_handler,
+            input_handle: input_handle,
+            tick_handle: tick_handle,
         }
     }
     // Call next function on hidden reveiver
